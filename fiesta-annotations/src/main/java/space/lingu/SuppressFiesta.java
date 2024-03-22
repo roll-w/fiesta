8ea1f02a8d11a488d229d47b2c367d481b879d0e
RollW <73376341+Roll-W@users.noreply.github.com>
2024-03-22 21:39:15 +0800

    feat: add SuppressFiesta annotation.

---
+++ fiesta-annotations/src/main/java/space/lingu/SuppressFiesta.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2022 Lingu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package space.lingu;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+/**
+ * Suppress Fiesta annotation messages.
+ * <p>
+ * For example, if you want to disable the {@link Warning} annotation,
+ * you can use this annotation to suppress it in this way:
+ * <pre>
+ * {@code
+ *     @SuppressFiesta(Warning.class)
+ *     public void doSomething() {
+ *     // ...
+ *     }
+ * }
+ * </pre>
+ *
+ * Note: {@code @SuppressFiesta(Fiesta.class)} will suppress all
+ * Fiesta annotations messages.
+ *
+ * @author RollW
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = {
+        CONSTRUCTOR, FIELD, LOCAL_VARIABLE, METHOD,
+        PACKAGE, PARAMETER, TYPE, TYPE_USE, TYPE_PARAMETER
+})
+public @interface SuppressFiesta {
+    /**
+     * The value of the suppression.
+     */
+    Class<?>[] value() default {};
+}
